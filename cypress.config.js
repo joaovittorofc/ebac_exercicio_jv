439103a92cb6ffdee24cde14579b732f1b3045ab
joaovittormkt <joaovittorvieira15@gmail.com>
2025-07-26 16:39:26 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    // Informe aqui a URL da sua aplicação local
     baseUrl: 'http://localhost:3000'
   }
-})
+});
